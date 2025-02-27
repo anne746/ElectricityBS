08f1161e6dc7de47aa19cdd28fe65c8c10e41bfb
anne746 <angielicaconson@gmail.com>
2025-02-28 02:47:54 +0800

    unsa nang tug?

---
+++ src/config/connectDB.java
@@ -0,0 +1,50 @@
+package config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class connectDB {
+    private Connection connect;
+    
+    public connectDB(){
+        try {
+            connect = DriverManager.getConnection("jdbc:mysql://localhost:3306/sasuman_ebs", "root", "");
+        } 
+        catch (SQLException ex) {
+            System.out.println("Can't connect to database: " + ex.getMessage());
+        }
+    }
+    
+    public Connection getConnection() {
+        return connect;
+    }
+    
+    public ResultSet getData(String sql) throws SQLException {
+        Statement stmt = connect.createStatement();
+        return stmt.executeQuery(sql);
+    }
+    
+    public int InsertData(String sql){
+        int result;
+        try{
+            PreparedStatement pst = connect.prepareStatement(sql);
+            pst.executeUpdate();
+            System.out.println("Inserted Successfully! ");
+            pst.close();
+            result = 1;
+        }catch(SQLException ex){
+            System.out.println("Connection Error: "+ex);
+            result = 0;
+        }
+        return result;
+    }
+
+    public PreparedStatement prepareStatement(String checkSql) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+        
+}
